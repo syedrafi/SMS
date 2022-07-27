494c0055af5f3190af119f85341e18c79a1c0e5b
Rafi <rafi@abws-MacBook-Pro.local>
2022-07-27 15:08:04 +0530

    added sms view

---
+++ SMS/SmsApp/SmsAppView.swift
@@ -0,0 +1,21 @@
+//
+//  SmsAppView.swift
+//  SMS
+//
+//  Created by Rafi on 27/07/22.
+//  Copyright © 2022 Rafi. All rights reserved.
+//
+
+import SwiftUI
+
+struct SmsAppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SmsAppView_Previews: PreviewProvider {
+    static var previews: some View {
+        SmsAppView()
+    }
+}
